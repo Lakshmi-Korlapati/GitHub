77e7a41e24930c45eb8ce7dc84bd8fbd518d290f
Lakshmi-Koprlapati <lakshmikorlapati342@gmail.com>
2024-04-17 09:56:55 +0530

    intial commit for svg

---
+++ githubProject/force-app/main/default/lwc/svglwc/svglwc.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class Svglwc extends LightningElement {
+    @track plotId;
+@track plotData;
+columns=[
+        {
+            label:'Plot Number',fieldName: 'value',type:'text'
+        }
+    ]
+
+
+    handleevent(){
+        this.plotId =event.target.id;
+        console.log(event.target);
+        this.plotData =[{value: this.plotId}];
+    }
+
+}
